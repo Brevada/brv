1fcad82242d2003333122c8a91c3e938dd5c2158
noahnu <noahnu@gmail.com>
2014-11-07 14:54:44 -0500

    Added user-defined tokens

--- pages/home/get_post_tokens.php
+++ pages/home/get_post_tokens.php
@@ -6,8 +6,16 @@ $arr = array();
 
 $query = Database::query("SELECT DISTINCT `name` FROM `posts` WHERE `name` IN (SELECT `name` FROM `posts` WHERE `name` LIKE '{$q}%' GROUP BY `name` HAVING COUNT(*) > 1) ORDER BY `name` ASC");
 
+$names = array();
+$names[] = $q;
+
+$arr[] = array('id' => $q, 'name' => $q);
+
 while($row = $query->fetch_assoc()){
-	$arr[] = array('id' => $row['name'], 'name' => $row['name']);
+	if(!in_array($row['name'], $names)){
+		$names[] = $row['name'];
+		$arr[] = array('id' => $row['name'], 'name' => $row['name']);
+	}
 }
 
 # JSON-encode the response
