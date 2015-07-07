ea0f72b0b47819d637cc29f859d496a3900ce0b6
noahnu <noahnu@gmail.com>
2015-07-06 21:03:56 -0400

    Mobile homepage.
    Mobile homepage and UTF-8 encoding in Controller.php

--- framework/Controller.php
+++ framework/Controller.php
@@ -11,6 +11,9 @@ define('URL', 'http://brevada.com/');
 session_start();
 //session_regenerate_id();
 
+mb_internal_encoding('UTF-8');
+mb_http_output('UTF-8');
+
 //header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 //header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 
