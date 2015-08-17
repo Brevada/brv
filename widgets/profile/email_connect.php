46c5be29d6c2378667bb3f5564dd3add74762151
noahnu <noahnu@gmail.com>
2015-08-17 15:31:07 -0400

    Toggle Email Request in Profile

--- widgets/profile/email_connect.php
+++ widgets/profile/email_connect.php
@@ -1,13 +1,19 @@
 <?php
 $store_id = $this->getParameter('store_id');
 $this->addResource('/js/communicate_pod.js');
+
+$store_id = @intval($store_id);
+
+$query = Database::query("SELECT `company_features`.`id` FROM `company_features` LEFT JOIN `companies` ON `companies`.`FeaturesID` = `company_features`.`id` LEFT JOIN `stores` ON `stores`.`CompanyID` = `companies`.`id` WHERE `stores`.`id` = {$store_id} AND `company_features`.EmailRequest IS NOT NULL AND `company_features`.EmailRequest = 1 LIMIT 1");
+$emailRequestEnabled = ($query !== false && $query->num_rows > 0);
 ?>
 
 <div class="thanks-header">
 	<h1><?php _e("Thanks for the feedback!"); ?></h1> 
-	<h2><?php _e("Let's stay in touch."); ?></h2>
+	<?php if($emailRequestEnabled){ ?><h2><?php _e("Let's stay in touch."); ?></h2><?php } ?>
 </div>
 
+<?php if($emailRequestEnabled){ ?>
 <div id="communicate_form">
 	 	<input type="hidden" name="store_id" value="<?php echo $store_id; ?>" />
 	 	<div class="input-group input-group-lg">
@@ -20,6 +26,7 @@ $this->addResource('/js/communicate_pod.js');
 	 	</div>
 	 <div id="finished" class="btn opt-out"><?php _e("I'd rather not."); ?></div>
 </div>
+<?php } ?>
 
  <div id="reset" class="refresh">
  	<i class="fa fa-refresh"></i>
