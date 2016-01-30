75427c76d848014357c3f5fe6f40440eca7712a8
NoahNU <noahnu@gmail.com>
2016-01-30 16:59:23 -0500

    BDFF loading. Aspects update.

--- js/Brevada.BDFF.js
+++ js/Brevada.BDFF.js
@@ -204,4 +204,15 @@
 		}
 	};
 	
+	bdff.equal = function(a, b){
+		if(a === b){ return true; }
+		if(a == null || b == null){ return true; }
+		if(a.length != b.length){ return false; }
+		
+		for (var i = 0; i < a.length; ++i) {
+			if (a[i] !== b[i]) return false;
+		}
+		return true;
+	};
+	
 })( window.bdff = window.bdff || {}, jQuery );
