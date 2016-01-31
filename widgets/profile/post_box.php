6787b83246336c86eaedcc0d55a9eaa040717d2f
NoahNU <noahnu@gmail.com>
2016-01-31 18:34:45 -0500

    Removed IPAddress check.
    Duplicates are identified by SESSION instead of IP.

--- widgets/profile/post_box.php
+++ widgets/profile/post_box.php
@@ -3,56 +3,31 @@ $this->addResource('/css/post_box.css');
 $this->addResource('/js/post_box.js');
 
 $r = $this->getParameter('row');
-
 $post_id = $r['ID'];
 $post_name = $r['Title'];
 $post_description = $r['Description'];
 
-$geo = Geography::GetGeo();
-$ipAddress = $geo['ip'];
-
-$userAgent = $_SERVER['HTTP_USER_AGENT'];
-
-/* Authorized tablet user agent. */
-$authUserAgent = TABLET_USERAGENT.'%';
-
-/*
-	Taken directly from /pages/overall/insert/insert_rating.php
-	(NOW() - INTERVAL 1 HOUR) determines how long a user must wait before rating again.
-*/
-$alreadyRated = true;
-if(($check = Database::prepare("SELECT `feedback`.id FROM `feedback` LEFT JOIN user_agents ON user_agents.ID = feedback.UserAgentID WHERE `feedback`.AspectID = ? AND `feedback`.IPAddress = ? AND (`feedback`.`Date` > NOW() - INTERVAL 1 HOUR) AND `user_agents`.UserAgent = ? AND `user_agents`.UserAgent NOT LIKE ? LIMIT 1")) !== false){
-	$check->bind_param('isss', $post_id, $ipAddress, $userAgent, $authUserAgent);
-	if($check->execute()){
-		$check->store_result();
-		if($check->num_rows == 0){
-			$alreadyRated = false;
-		}
-		$check->close();
-	}
-}
+$alreadyRated = isset($_SESSION['feedback']) && !empty($_SESSION['feedback']) && in_array($post_id, $_SESSION['feedback']);
 ?>
 
 <div id="aspect_<?php echo $post_id; ?>" class="aspect<?php echo $alreadyRated ? ' rated' : ''; ?>">
-		<div class="aspect-header">
-			<div class="title"><?php _e($post_name); ?></div>
-			<!-- <div class="subtitle"><?php echo substr(__($post_description),0,85); if(strlen(__($post_description))>85){echo '...';} ?></div> -->
-		</div>
-
-		<div class="rating-box">
-			<?php				
-			if(!$alreadyRated){
-			?>
-			<div><?php $this->add(new View('../widgets/profile/star_rating_bar.php', array('row' => $r))); ?></div>
-			<br style="clear: both;" />
-			<div class="rate-description">
-				<div class="pull-left"><?php _e('Worst'); ?></div>
-				<div class="pull-right"><?php _e('Best'); ?></div>
-				<br class="clear" />
-			</div>
-			<?php } else { ?>
-			<div class="appear" id="appear_bar_<?php echo $post_id; ?>"  align="center" style="width:100%;"><?php _e('Already rated.'); ?></div>	
-			<?php } ?>
-		</div>
-
+	<div class="aspect-header">
+		<div class="title"><?php _e($post_name); ?></div>
+		<?php echo substr(__($post_description),0,85); if(strlen(__($post_description))>85){echo '...';} ?>
 	</div>
+	<div class="rating-box">
+		<?php				
+		if(!$alreadyRated){
+		?>
+		<div><?php $this->add(new View('../widgets/profile/star_rating_bar.php', array('row' => $r))); ?></div>
+		<br style="clear: both;" />
+		<div class="rate-description">
+			<div class="pull-left"><?php _e('Worst'); ?></div>
+			<div class="pull-right"><?php _e('Best'); ?></div>
+			<br class="clear" />
+		</div>
+		<?php } else { ?>
+		<div class="appear" id="appear_bar_<?php echo $post_id; ?>"  align="center" style="width:100%;"><?php _e('Already rated.'); ?></div>	
+		<?php } ?>
+	</div>
+</div>
