c258c3d8308fedcdca4cdd54788e7d047e2ad757
NoahNU <noahnu@gmail.com>
2016-07-13 22:42:03 -0400

    Customer Emails and Custom aspects; settings.

--- pages/settings/section_feedback.php
+++ pages/settings/section_feedback.php
@@ -7,12 +7,14 @@ $message = '';
 
 if((!empty($_SESSION['StoreID']) && (($_SESSION['Corporate'] && Permissions::has(Permissions::MODIFY_COMPANY_STORES)) || !$_SESSION['Corporate'] && Permissions::has(Permissions::MODIFY_STORE))) || (!empty($_POST['ddStores']) && $_SESSION['Corporate'] && Permissions::has(Permissions::MODIFY_COMPANY_STORES))){
 	
+	$company_id = Brevada::validate($_SESSION['CompanyID'], VALIDATE_DATABASE);
+	
 	$store_id = '';
 	if(empty($_POST['ddStores'])){	
 		$store_id = Brevada::validate($_SESSION['StoreID'], VALIDATE_DATABASE);
 	} else {
 		$store_id = Brevada::validate(Brevada::FromPOST('ddStores'), VALIDATE_DATABASE);
-		$check = Database::query("SELECT `stores`.`id` FROM `stores` WHERE `stores`.`CompanyID` = {$_SESSION['CompanyID']} AND `stores`.`id` = {$store_id} LIMIT 1");
+		$check = Database::query("SELECT `stores`.`id` FROM `stores` WHERE `stores`.`CompanyID` = {$company_id} AND `stores`.`id` = {$store_id} LIMIT 1");
 		if($check === false || $check->num_rows == 0 || !Permissions::has(Permissions::MODIFY_COMPANY_STORES)){
 			unset($_POST);
 			Brevada::Redirect('/settings?section=feedback');
@@ -21,6 +23,36 @@ if((!empty($_SESSION['StoreID']) && (($_SESSION['Corporate'] && Permissions::has
 
 	if(isset($_POST) && isset($_POST['posts-token'])){
 		$posts_tokens = explode(',', $_POST['posts-token']);
+	}
+	
+	if(isset($_POST) && !empty($_POST['txtCustomAspect'])){
+		$custom = trim($_POST['txtCustomAspect']);
+		if(!empty($custom)){			
+			// If title exists, don't allow.
+			if(($stmt = Database::prepare("SELECT FROM `aspect_type` WHERE `Title` = ? AND (`CompanyID` = ? OR `CompanyID` IS NULL) LIMIT 1")) !== false){
+				$stmt->bind_param('si', $custom, $company_id);
+				if($stmt->execute()){
+					$stmt->store_result();
+					if($stmt->num_rows > 0){
+						$message = __("An aspect with this name already exists.");
+					}
+				}
+				$stmt->close();
+			}
+			
+			if(empty($message) && ($stmt = Database::prepare("INSERT INTO `aspect_type` (`Title`, `CompanyID`) VALUES (?, ?)")) !== false){
+				$stmt->bind_param('si', $custom, $company_id);
+				if($stmt->execute()){
+					$message = "Custom aspect created.";
+					$posts_tokens[] = Database::getCon()->insert_id;
+				} else {
+					$message = "Failed to create custom aspect.";
+				}
+			}
+		}
+	}
+	
+	if(isset($_POST) && isset($_POST['posts-token'])){
 		if($posts_tokens !== false){
 
 			if(($query = Database::query("SELECT aspects.ID FROM aspects WHERE aspects.StoreID = {$store_id} AND aspects.`Active` = 1")) !== false){
@@ -34,7 +66,15 @@ if((!empty($_SESSION['StoreID']) && (($_SESSION['Corporate'] && Permissions::has
 			foreach($posts_tokens as $token){
 				$token = Brevada::validate($token, VALIDATE_DATABASE);
 				if(!empty($token)){
-					if(($query = Database::query("SELECT aspects.`Active` FROM aspects WHERE aspects.StoreID = {$store_id} AND aspects.AspectTypeID = {$token} LIMIT 1")) !== false){
+					if(($query = Database::query("
+						SELECT aspects.`Active` FROM aspects
+						JOIN aspect_type ON aspect_type.id = aspects.AspectTypeID
+						WHERE 
+							aspects.StoreID = {$store_id} AND 
+							aspects.AspectTypeID = {$token} AND
+							(aspect_type.CompanyID IS NULL OR aspect_type.CompanyID = {$company_id})
+						LIMIT 1
+						")) !== false){
 						if($query->num_rows > 0){
 							Database::query("UPDATE aspects SET aspects.`Active` = 1 WHERE aspects.StoreID = {$store_id} AND aspects.AspectTypeID = {$token} LIMIT 1");
 						} else {
@@ -64,19 +104,67 @@ if((!empty($_SESSION['StoreID']) && (($_SESSION['Corporate'] && Permissions::has
 <div class='form-account'>
 	<?php if(!empty($message)){ echo "<p class='message'>{$message}</p>"; } ?>
 	<?php if(!empty($_SESSION['StoreID']) && (($_SESSION['Corporate'] && Permissions::has(Permissions::MODIFY_COMPANY_STORES)) || !$_SESSION['Corporate'] && Permissions::has(Permissions::MODIFY_STORE))){ ?>
-	<span class="form-header"><?php _e('What do you want to get feedback on?'); ?></span>
+	<span class="form-header"><?php _e('What do you want to get feedback on?'); ?><span class='pull-right'><i class='fa fa-info-circle help' data-tooltip="<?php _e("You can toggle aspects on and off without worrying about losing data."); ?>"></i></span></span>
 	<div class='token-container'>
 		<div class='tokens'>
 			<?php
-			if(($query = Database::query("SELECT aspect_type.Title, aspect_type.ID as AspectTypeID, (SELECT aspects.id FROM `aspects` WHERE `aspects`.AspectTypeID = aspect_type.ID AND `aspects`.Active = 1 AND  `aspects`.StoreID = {$store_id}) as `Selected` FROM aspect_type ORDER BY aspect_type.Title ASC")) !== false){
-				while($row = $query->fetch_assoc()){
-					echo "<div class='token".(empty($row['Selected']) ? ' noselect' : ' selected')."' data-tokenid='{$row['AspectTypeID']}'><span>".__($row['Title'])."</span></div>";
+			if ($stmt = Database::prepare("
+				SELECT aspect_type.Title, aspect_type.ID as AspectTypeID,
+				(SELECT aspects.id FROM `aspects` WHERE `aspects`.AspectTypeID = aspect_type.ID AND `aspects`.Active = 1 AND `aspects`.StoreID = ?) as `Selected`
+				FROM aspect_type
+				WHERE `CompanyID` IS NULL
+				ORDER BY aspect_type.Title ASC
+			")) {
+				$stmt->bind_param('i', $store_id);
+				if ($stmt->execute()){
+					$stmt->store_result();
+					$stmt->bind_result($aspect_title, $aspect_id, $aspect_selected);
+					while ($stmt->fetch()){
+						echo "<div class='token".(empty($aspect_selected) ? ' noselect' : ' selected')."' data-tokenid='{$aspect_id}'><span>".__($aspect_title)."</span></div>";
+					}
 				}
 			}
 			?>
 			<input type='hidden' name='posts-token' id='tokens' />
 		</div>
 	</div>
+	<br /><br />
+	<span class="form-header"><?php _e('Custom aspects:'); ?><span class='pull-right'><i class='fa fa-info-circle help' data-tooltip="<?php _e("Custom aspects are unique to your company and can contain branding and store-specific information."); ?>"></i></span></span>
+	<?php $no_custom = true; ?>
+	<div class='token-container'>
+		<div class='tokens'>
+			<?php
+			if ($stmt = Database::prepare("
+				SELECT aspect_type.Title, aspect_type.ID as AspectTypeID,
+				(SELECT aspects.id FROM `aspects` WHERE `aspects`.AspectTypeID = aspect_type.ID AND `aspects`.Active = 1 AND `aspects`.StoreID = ?) as `Selected`
+				FROM aspect_type
+				WHERE `CompanyID` = ?
+				ORDER BY aspect_type.Title ASC
+			")) {
+				$company_id = $_SESSION['CompanyID'];
+				$stmt->bind_param('ii', $store_id, $company_id);
+				if ($stmt->execute()){
+					$stmt->store_result();
+					$stmt->bind_result($aspect_title, $aspect_id, $aspect_selected);
+					while ($stmt->fetch()){
+						$no_custom = false;
+						echo "<div class='token".(empty($aspect_selected) ? ' noselect' : ' selected')."' data-tokenid='{$aspect_id}'><span>".__($aspect_title)."</span></div>";
+					}
+				}
+			}
+			?>
+		</div>
+	</div>
+	<?php if ($no_custom){ ?>
+	<div class='well well-sm'><?php _e("You currently have no custom aspects."); ?></div>
+	<?php } ?>
+	<br />
+	
+	<div class='form-group'>
+		<span class="form-header"><?php _e("Create a new custom aspect:");?><span class='pull-right'><i class='fa fa-info-circle help' data-tooltip="<?php _e("For example, if you have a signature dish, you can name it.<br/><br/>Keep in mind, the aspect name is case-sensitive."); ?>"></i></span></span>
+		<input type='text' class='form-control' name='txtCustomAspect' placeholder="<?php _e("e.g. Signature Dish"); ?>" />
+	</div>
+	
 	<div id="submit" class="submit-next"><?php _e('Save'); ?></div>
 	<?php } else if($_SESSION['Corporate'] && Permissions::has(Permissions::MODIFY_COMPANY_STORES)) { ?>
 	<span class="form-header"><?php _e('What do you want to get feedback on?'); ?></span>
@@ -112,6 +200,43 @@ if((!empty($_SESSION['StoreID']) && (($_SESSION['Corporate'] && Permissions::has
 			<input type='hidden' name='posts-token' id='tokens' />
 		</div>
 	</div>
+	
+	<br /><br />
+	<span class="form-header"><?php _e('Custom aspects:'); ?><span class='pull-right'><i class='fa fa-info-circle help' data-tooltip="<?php _e("Custom aspects are unique to your company and can contain branding and store-specific information."); ?>"></i></span></span>
+	<?php $no_custom = true; ?>
+	<div class='token-container'>
+		<div class='tokens'>
+			<?php
+			if ($stmt = Database::prepare("
+				SELECT aspect_type.Title, aspect_type.ID as AspectTypeID,
+				(SELECT aspects.id FROM `aspects` WHERE `aspects`.AspectTypeID = aspect_type.ID AND `aspects`.Active = 1 AND `aspects`.StoreID = ?) as `Selected`
+				FROM aspect_type
+				WHERE `CompanyID` = ?
+				ORDER BY aspect_type.Title ASC
+			")) {
+				$company_id = $_SESSION['CompanyID'];
+				$stmt->bind_param('ii', $store_id, $company_id);
+				if ($stmt->execute()){
+					$stmt->store_result();
+					$stmt->bind_result($aspect_title, $aspect_id, $aspect_selected);
+					while ($stmt->fetch()){
+						$no_custom = false;
+						echo "<div class='token".(empty($aspect_selected) ? ' noselect' : ' selected')."' data-tokenid='{$aspect_id}'><span>".__($aspect_title)."</span></div>";
+					}
+				}
+			}
+			?>
+		</div>
+	</div>
+	<?php if ($no_custom){ ?>
+	<div class='well well-sm'><?php _e("You currently have no custom aspects."); ?></div>
+	<?php } ?>
+	<br />
+	<div class='form-group'>
+		<span class="form-header"><?php _e("Create a new custom aspect:");?><span class='pull-right'><i class='fa fa-info-circle help' data-tooltip="<?php _e("For example, if you have a signature dish, you can name it.<br/><br/>Keep in mind, the aspect name is case-sensitive."); ?>"></i></span></span>
+		<input type='text' class='form-control' name='txtCustomAspect' placeholder="<?php _e("e.g. Signature Dish"); ?>" />
+	</div>
+	
 	<div id="submit" class="submit-next"><?php _e('Save'); ?></div>
 	<?php } ?>
 	<?php } ?>
