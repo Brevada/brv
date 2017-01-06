b3c98e4a8b58b4f653f95e95e05ce7d548b48524
NoahNU <noahnu@gmail.com>
2017-01-06 14:01:42 -0500

    Moved legacy app to subdirectory. Beginning of cleanup/restructure.

---
+++ legacy/framework/classes/APITasks/Task.Resources.class.php
@@ -0,0 +1,33 @@
+<?php
+class TaskResources extends AbstractTask
+{
+	public function execute($method, $tasks, &$data)
+	{
+		if($method == 'get'){
+			if(TaskLoader::requiresData(['serial'], $_GET)){
+				$serial = $_GET['serial'];
+				
+				$url = URL;
+				$url = "http://192.168.1.115:81/";
+				
+				$data['download'] = array();
+				$data['download'][] = array('name' => 'layout.css', 'url' => 'css/layout.css', 'role' => 'styles');
+				$data['download'][] = array('name' => 'profile_header.css', 'url' => 'css/profile_header.css', 'role' => 'styles');
+				$data['download'][] = array('name' => 'post_box.css', 'url' => 'css/post_box.css', 'role' => 'styles');
+				$data['download'][] = array('name' => 'star_rating_bar.css', 'url' => 'css/star_rating_bar.css', 'role' => 'styles');
+				$data['download'][] = array('name' => 'tablet.js', 'url' => 'js/tablet.js', 'role' => 'script');
+				$data['download'][] = array('name' => 'profile.css', 'url' => 'css/profile.css', 'role' => 'styles');
+				$data['download'][] = array('name' => 'profile.html', 'url' => 'api/profile?m=' . $serial, 'role' => 'markup');
+				
+				foreach($data['download'] as &$dl){
+					$dl['url'] = $url . $dl['url'];
+					$dl['sha1'] = strtoupper(sha1_file($dl['url']));
+				}
+				unset($dl);
+			} else {
+				throw new Exception("Incomplete request.");
+			}
+		}
+	}
+}
+?>
