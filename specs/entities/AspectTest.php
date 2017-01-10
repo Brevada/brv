a9d2ed01c4788b48a1ab4409bcdf959e096ef3ef
NoahNU <noahnu@gmail.com>
2017-01-09 20:13:48 -0500

    Created files for entity specs.

---
+++ specs/entities/AspectTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * AspectTest | TestCase
+ *
+ * @version v0.0.1 (Jan. 09, 2017)
+ * @copyright Copyright (c) 2017, Brevada
+ */
+
+use PHPUnit\Framework\TestCase;
+
+use Brv\core\libs\database\TestDatabase;
+use Brv\core\libs\database\Database;
+
+/**
+ *
+ */
+class AspectTest extends TestCase
+{
+    /**
+     * Sets up database with base schema.
+     * @beforeClass
+     *
+     * @throws \Exception if database cannot be setup.
+     */
+    public static function setUpBeforeClass() {
+        $result = TestDatabase::executeFile(realpath(dirname(__FILE__) . '/schema.sql'));
+        if ($result !== true) throw new \Exception("Failed to setup database for tests.");
+    }
+
+    /**
+     * Truncates database.
+     * @afterClass
+     *
+     * @throws \Exception if database cannot be truncated.
+     */
+    public static function tearDownAfterClass() {
+        $result = TestDatabase::executeFile(realpath(dirname(__FILE__) . '/dropall.sql'));
+        if ($result !== true) throw new \Exception("Failed to setup database for tests.");
+    }
+}
+?>
