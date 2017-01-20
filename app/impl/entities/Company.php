1830574b5bba1bad2dfa1277539aee2a5a6931d4
NoahNU <noahnu@gmail.com>
2017-01-20 16:05:32 -0500

    Company entity broad tests.

--- app/impl/entities/Company.php
+++ app/impl/entities/Company.php
@@ -41,7 +41,17 @@ class Company extends Entity
     public static function queryId($id)
     {
         try {
-            $stmt = DB::get()->prepare("SELECT * FROM companies WHERE id = :id");
+            $stmt = DB::get()->prepare("
+            SELECT companies.*,
+                UNIX_TIMESTAMP(companies.DateCreated) AS sDateCreated,
+                UNIX_TIMESTAMP(companies.ExpiryDate) AS sDateExpiry,
+                company_features.MaxTablets, company_features.MaxStores,
+                company_features.MaxAccounts
+            FROM companies
+            LEFT JOIN company_features ON company_features.id = companies.FeaturesID
+            WHERE companies.id = :id
+            LIMIT 1
+            ");
             $stmt->bindValue(':id', (int) $id, \PDO::PARAM_INT);
             $stmt->execute();
             if (($row = $stmt->fetch(\PDO::FETCH_ASSOC)) !== false) {
@@ -64,7 +74,13 @@ class Company extends Entity
     {
         try {
             $stmt = DB::get()->prepare("
-                SELECT companies.* FROM companies
+                SELECT companies.*,
+                    UNIX_TIMESTAMP(companies.DateCreated) AS sDateCreated,
+                    UNIX_TIMESTAMP(companies.ExpiryDate) AS sDateExpiry,
+                    company_features.MaxTablets, company_features.MaxStores,
+                    company_features.MaxAccounts
+                FROM companies
+                LEFT JOIN company_features ON company_features.id = companies.FeaturesID
                 JOIN accounts ON accounts.CompanyID = companies.id
                 WHERE accounts.id = :id
                 LIMIT 1
@@ -81,6 +97,113 @@ class Company extends Entity
         return null;
     }
 
+    /* Instance Methods */
+
+    /**
+     * Commits Company to database.
+     *
+     * @return integer The id of the newly created/updated Company.
+     */
+    public function commit()
+    {
+        if ($this->has('id') && $this->get('id') !== -1) {
+            /*
+             * UPDATE rather than INSERT.
+             */
+             try {
+                 $stmt = DB::get()->prepare("
+                     UPDATE company_features SET
+                     MaxTablets = :maxTablets,
+                     MaxAccounts = :maxAccounts,
+                     MaxStores = :maxStores
+                     WHERE id = :features_id
+                 ");
+                 $stmt->bindValue(':features_id', $this->getFeaturesId(), \PDO::PARAM_INT);
+                 $stmt->bindValue(':maxTablets', $this->getMaxTablets(), \PDO::PARAM_INT);
+                 $stmt->bindValue(':maxAccounts', $this->getMaxAccounts(), \PDO::PARAM_INT);
+                 $stmt->bindValue(':maxStores', $this->getMaxStores(), \PDO::PARAM_INT);
+                 $stmt->execute();
+             } catch (\PDOException $ex) {
+                 \App::log()->error($ex->getMessage());
+             }
+
+            try {
+                $stmt = DB::get()->prepare("
+                    UPDATE companies SET
+                    Name = :name, Active = :active, Website = :website,
+                    PhoneNumber = :phone, ExpiryDate = FROM_UNIXTIME(:expiry),
+                    CategoryID = :category_id
+                    WHERE id = :id
+                ");
+                $stmt->bindValue(':id', $this->getId(), \PDO::PARAM_INT);
+                $stmt->bindValue(':name', $this->getName(), \PDO::PARAM_STR);
+                $stmt->bindValue(':active', (int) $this->isActive(), \PDO::PARAM_INT);
+                $stmt->bindValue(':website', $this->getWebsite(), \PDO::PARAM_STR);
+                $stmt->bindValue(':phone', $this->getPhoneNumber(), \PDO::PARAM_STR);
+                $stmt->bindValue(':expiry', $this->getExpiryDate(), \PDO::PARAM_INT);
+                $stmt->bindValue(':category_id', $this->getCategoryId(), \PDO::PARAM_INT);
+                $stmt->execute();
+                return $this->getId();
+            } catch (\PDOException $ex) {
+                \App::log()->error($ex->getMessage());
+            }
+
+            return null;
+        } else {
+            /* Completely new company. INSERT. */
+            try {
+                DB::get()->beginTransaction();
+
+                // First create Company Features entry.
+                $stmt = DB::get()->prepare("
+                    INSERT INTO company_features SET
+                    MaxTablets = :maxTablets, MaxAccounts = :maxAccounts,
+                    MaxStores = :maxStores
+                ");
+                $stmt->bindValue(':maxTablets', $this->getMaxTablets(), \PDO::PARAM_INT);
+                $stmt->bindValue(':maxAccounts', $this->getMaxAccounts(), \PDO::PARAM_INT);
+                $stmt->bindValue(':maxStores', $this->getMaxStores(), \PDO::PARAM_INT);
+                $stmt->execute();
+                $this->set('FeaturesID', (int) DB::get()->lastInsertId());
+
+                $this->set('sDateCreated', time());
+
+                // Create company and link to features.
+                $stmt = DB::get()->prepare("
+                    INSERT INTO companies SET
+                    Name = :name, Active = :active, Website = :website,
+                    PhoneNumber = :phone, ExpiryDate = FROM_UNIXTIME(:expiry),
+                    CategoryID = :category_id, FeaturesID = :features_id,
+                    DateCreated = FROM_UNIXTIME(:now)
+                ");
+                $stmt->bindValue(':name', $this->getName(), \PDO::PARAM_STR);
+                $stmt->bindValue(':active', (int) $this->isActive(), \PDO::PARAM_INT);
+                $stmt->bindValue(':website', $this->getWebsite(), \PDO::PARAM_STR);
+                $stmt->bindValue(':phone', $this->getPhoneNumber(), \PDO::PARAM_STR);
+                $stmt->bindValue(':expiry', $this->getExpiryDate(), \PDO::PARAM_INT);
+                $stmt->bindValue(':category_id', $this->getCategoryId(), \PDO::PARAM_INT);
+                $stmt->bindValue(':features_id', $this->getFeaturesId(), \PDO::PARAM_INT);
+                $stmt->bindValue(':now', $this->getDateCreated(), \PDO::PARAM_INT);
+                $stmt->execute();
+                $this->set('id', (int) DB::get()->lastInsertId());
+
+                DB::get()->commit();
+
+                return $this->getId();
+            } catch (\PDOException $ex) {
+                \App::log()->error($ex->getMessage());
+            }
+
+            try {
+                DB::get()->rollBack();
+            } catch (\PDOException $ex) {
+                \App::log()->error($ex->getMessage());
+            }
+        }
+
+        return null;
+    }
+
     /**
      * Gets the company id.
      *
@@ -89,6 +212,232 @@ class Company extends Entity
     public function getId()
     {
         return (int) $this->get('id');
+    }
+
+    /**
+     * Gets the company's category id.
+     *
+     * @return integer
+     */
+    public function getCategoryId()
+    {
+        if ($this->get('CategoryID') === null) return null;
+        return (int) $this->get('CategoryID');
+    }
+
+    /**
+     * Sets the company's category id.
+     *
+     * @param integer $id The new category id.
+     * @return integer
+     */
+    public function setCategoryId($id)
+    {
+        $this->set('CategoryID', (int) $id);
+        return $this->getCategoryId();
+    }
+
+    /**
+     * Gets the date the company created an "account" (the company entry in
+     * the schema).
+     *
+     * @return integer The date created in unix seconds.
+     */
+    public function getDateCreated()
+    {
+        return (int) $this->get('sDateCreated');
+    }
+
+    /**
+     * Sets the date the company's plan will expire.
+     *
+     * @param integer $seconds The unix timestamp to set the expiry date to or offset.
+     * @param boolean $isOffset Indicates if the $seconds should be added to the
+     * current time.
+     *
+     * @return integer The date in unix seconds.
+     */
+    public function setExpiryDate($seconds, $isOffset = false)
+    {
+        $this->set('sDateExpiry', ($isOffset ? time() : 0) + (int) $seconds);
+        return $this->getExpiryDate();
+    }
+
+    /**
+     * Gets the date the company's plan will expire.
+     *
+     * @return integer The date in unix seconds.
+     */
+    public function getExpiryDate()
+    {
+        return (int) $this->get('sDateExpiry');
+    }
+
+    /**
+     * Gets the company name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->get('Name');
+    }
+
+    /**
+     * Sets the company name.
+     *
+     * @param string $name
+     * @return string
+     */
+    public function setName($name)
+    {
+        $this->set('Name', $name);
+        return $this->getName();
+    }
+
+    /**
+     * Checks if the company is active.
+     *
+     * @return boolean
+     */
+    public function isActive()
+    {
+        return $this->get('Active') == 1;
+    }
+
+    /**
+     * Sets the active state of the company.
+     *
+     * @param boolean $state The new active state.
+     * @return boolean
+     */
+    public function setActive($state)
+    {
+        $this->set('Active', (int) $state);
+        return $this->isActive();
+    }
+
+    /**
+     * Gets the company's phone number.
+     *
+     * @return string
+     */
+    public function getPhoneNumber()
+    {
+        return $this->get('PhoneNumber');
+    }
+
+    /**
+     * Sets the company's phone number.
+     *
+     * @param string $phone The new phone number.
+     * @return string
+     */
+    public function setPhoneNumber($phone)
+    {
+        $this->set('PhoneNumber', $phone);
+        return $this->getPhoneNumber();
+    }
+
+    /**
+     * Gets the company's website.
+     *
+     * @return string
+     */
+    public function getWebsite()
+    {
+        return $this->get('Website');
+    }
+
+    /**
+     * Sets the company's website.
+     *
+     * @param string $website The new website.
+     * @return string
+     */
+    public function setWebsite($website)
+    {
+        $this->set('Website', $website);
+        return $this->getWebsite();
+    }
+
+    /**
+     * Gets the max number of stores the company can have.
+     *
+     * @return integer
+     */
+    public function getMaxStores()
+    {
+        return (int) $this->get('MaxStores', 0);
+    }
+
+    /**
+     * Sets the max number of stores the company can have.
+     *
+     * @param integer $val The new value.
+     * @return integer
+     */
+    public function setMaxStores($val)
+    {
+        $this->set('MaxStores', (int) $val);
+        return $this->getMaxStores();
+    }
+
+    /**
+     * Gets the max number of accounts the company can have.
+     *
+     * @return integer
+     */
+    public function getMaxAccounts()
+    {
+        return (int) $this->get('MaxAccounts', 0);
+    }
+
+    /**
+     * Sets the max number of accounts the company can have.
+     *
+     * @param integer $val The new value.
+     * @return integer
+     */
+    public function setMaxAccounts($val)
+    {
+        $this->set('MaxAccounts', (int) $val);
+        return $this->getMaxAccounts();
+    }
+
+    /**
+     * Gets the max number of tablets the company can have.
+     *
+     * @return integer
+     */
+    public function getMaxTablets()
+    {
+        return (int) $this->get('MaxTablets', 0);
+    }
+
+    /**
+     * Sets the max number of tablets the company can have.
+     *
+     * @param integer $val The new value.
+     * @return integer
+     */
+    public function setMaxTablets($val)
+    {
+        $this->set('MaxTablets', (int) $val);
+        return $this->getMaxTablets();
+    }
+
+    /**
+     * Gets the id of the features table associated with the company.
+     *
+     * @return integer
+     */
+    public function getFeaturesId()
+    {
+        if ($this->get('FeaturesID') === null) {
+            return null;
+        }
+        return (int) $this->get('FeaturesID');
     }
 
     /**
--- specs/entities/CompanyTest.php
+++ specs/entities/CompanyTest.php
@@ -11,6 +11,10 @@ use PHPUnit\Framework\TestCase;
 use Brv\core\libs\database\TestDatabase;
 use Brv\core\libs\database\Database;
 
+use Brv\impl\entities\Company;
+
+require_once 'app/App.php';
+
 /**
  *
  */
@@ -22,9 +26,125 @@ class CompanyTest extends TestCase
      *
      * @throws \Exception if database cannot be setup.
      */
-    public static function setUpBeforeClass() {
+    public static function setUpBeforeClass()
+    {
         $result = TestDatabase::executeFile(realpath(dirname(__FILE__) . '/../schema/schema.sql'));
-        if ($result !== true) throw new \Exception("Failed to setup database for tests.");
+        if ($result !== true) {
+            throw new \Exception("Failed to setup database for tests.");
+        }
+    }
+
+    /**
+     * Tests that the "set" data is not altered.
+     * @test
+     */
+    public function testImmutableData()
+    {
+        $name = 'Test Company';
+        $active = true;
+        $website = 'www.example.com';
+        $phone = '123456789';
+        $expiry = time();
+
+        $company = new Company();
+        $company->setName($name);
+        $company->setActive($active);
+        $company->setWebsite($website);
+        $company->setPhoneNumber($phone);
+        $company->setExpiryDate($expiry);
+
+        $this->assertEquals($name, $company->getName());
+        $this->assertEquals($active, $company->isActive());
+        $this->assertEquals($website, $company->getWebsite());
+        $this->assertEquals($phone, $company->getPhoneNumber());
+        $this->assertEquals($expiry, $company->getExpiryDate());
+    }
+
+    /**
+     * Tests the creation of a company.
+     * @test
+     */
+    public function testCreateCompany()
+    {
+        $name = 'Test Company';
+        $active = true;
+        $website = 'www.example.com';
+        $phone = '123456789';
+        $expiry = time();
+
+        $company = new Company();
+        $company->setName($name);
+        $company->setActive($active);
+        $company->setWebsite($website);
+        $company->setPhoneNumber($phone);
+        $company->setExpiryDate($expiry);
+        $id = $company->commit();
+
+        $this->assertNotNull($id, "Company insertion failed.");
+    }
+
+    /**
+     * Tests that the data persists in the database and can be recalled.
+     * @test
+     */
+    public function testPersistantData()
+    {
+        $name = 'Test Company';
+        $active = true;
+        $website = 'www.example.com';
+        $phone = '123456789';
+        $expiry = time();
+
+        $company = new Company();
+        $company->setName($name);
+        $company->setActive($active);
+        $company->setWebsite($website);
+        $company->setPhoneNumber($phone);
+        $company->setExpiryDate($expiry);
+        $id = $company->commit();
+
+        $recalled = Company::queryId($id);
+
+        $this->assertEquals($name, $recalled->getName());
+        $this->assertEquals($active, $recalled->isActive());
+        $this->assertEquals($website, $recalled->getWebsite());
+        $this->assertEquals($phone, $recalled->getPhoneNumber());
+        $this->assertEquals($expiry, $recalled->getExpiryDate());
+        $this->assertTrue(abs($expiry - $recalled->getDateCreated()) < 100);
+    }
+
+    /**
+     * Tests update.
+     * @test
+     */
+    public function testUpdate()
+    {
+        $name = 'Test Company';
+        $active = true;
+        $website = 'www.example.com';
+        $phone = '123456789';
+        $expiry = time();
+
+        // INSERT
+        $company = new Company();
+        $company->setName($name);
+        $company->setActive($active);
+        $company->setWebsite($website);
+        $company->setPhoneNumber($phone);
+        $company->setExpiryDate($expiry);
+        $id = $company->commit();
+
+        // UPDATE
+        $newName = 'New Name';
+        $company->setName($newName);
+        $updatedId = $company->commit();
+
+        $this->assertEquals($id, $updatedId);
+
+        // Refresh
+        $company = Company::queryId($company->getId());
+
+        $this->assertEquals($newName, $company->getName());
     }
 
     /**
@@ -33,9 +153,11 @@ class CompanyTest extends TestCase
      *
      * @throws \Exception if database cannot be truncated.
      */
-    public static function tearDownAfterClass() {
+    public static function tearDownAfterClass()
+    {
         $result = TestDatabase::executeFile(realpath(dirname(__FILE__) . '/../schema/dropall.sql'));
-        if ($result !== true) throw new \Exception("Failed to setup database for tests.");
+        if ($result !== true) {
+            throw new \Exception("Failed to setup database for tests.");
+        }
     }
 }
-?>
