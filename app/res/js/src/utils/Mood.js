e651caf92d9828287a94a31a648066c77ed27738
NoahNU <noahnu@gmail.com>
2017-02-12 17:04:09 -0500

    Removed redundant colour function. Added docstring

--- app/res/js/src/utils/Mood.js
+++ app/res/js/src/utils/Mood.js
@@ -1,9 +1,13 @@
+/**
+ * Gets the mood CSS class corresponding to a value within a range.
+ *
+ * @param {number} val
+ * @param {number} [min=0]
+ * @param {number} [max=100]
+ * @return {string}
+ */
 const Mood = (val, min=0, max=100) => {
     return 'mood-' + (Math.round(((parseFloat(val) + Math.abs(min)) * 100 / (max-min))/10)*10);
 };
 
-const MoodColor = (mood) => {
-    return '#ff00ff';
-};
-
-export { Mood, MoodColor };
+export { Mood };
