538c9c1503d578655fdb53553061ac708f4751e1
NoahNU <noahnu@gmail.com>
2017-03-03 11:15:20 -0500

    Separated vendor chunk. Added shorthands per: https://github.com/lodash/lodash-webpack-plugin/issues/9

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,12 +4,22 @@ var devConfig = require('./webpack.config');
 
 module.exports = Object.assign(devConfig, {
     'plugins': [
-        new webpack.optimize.CommonsChunkPlugin('chunks'),
+        new webpack.optimize.CommonsChunkPlugin({
+            /* Don't add content to vendor and globals. */
+            names: ['vendor'].concat(Object.keys(devConfig.entry).filter(
+                p => p.indexOf('globals/') === 0
+            )),
+            minChunks: Infinity
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'chunks'
+        }),
         new webpack.optimize.UglifyJsPlugin(),
         new webpack.optimize.AggressiveMergingPlugin(),
         new webpack.optimize.OccurrenceOrderPlugin(),
         new LodashModuleReplacementPlugin({
             collections: true,
+            shorthands: true,
             paths: true
         })
     ]
