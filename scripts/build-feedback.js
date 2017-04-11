35d29a4c861c66dc76de2234c919cae8532f751c
NoahNU <noahnu@gmail.com>
2017-04-11 14:30:07 -0400

    added ordering of feedback deps

--- scripts/build-feedback.js
+++ scripts/build-feedback.js
@@ -7,7 +7,9 @@ const del = require('del');
 
 const config = {
     entry: {
-        feedback: path.join(__dirname, '..', 'app', 'res', 'js', 'src', 'views', 'feedback.js')
+        /* We enforce ordering by numbering entries. */
+        '0_feedback_brv': path.join(__dirname, '..', 'app', 'res', 'js', 'src', 'globals', 'feedback.js'),
+        '1_feedback_view': path.join(__dirname, '..', 'app', 'res', 'js', 'src', 'views', 'feedback.js')
     },
     output: {
         path: path.join(__dirname, '..', 'app', 'resp', 'feedback'),
